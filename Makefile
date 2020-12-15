5e72307b1e2b6a18008666aef9173d0645866fa6
Andrew Tropin <andrew@trop.in>
2020-12-15 15:01:23 +0300

    Add channels, Makefile and ixy

---
+++ Makefile
@@ -0,0 +1,2 @@
+iso:
+	guix time-machine -C ./channels.scm -- system disk-image ./ixy.scm
---
+++ channels.scm
@@ -0,0 +1,6 @@
+(list (channel
+        (name 'rde)
+        (url "https://github.com/abcdw/rde.git"))
+      (channel
+       (name 'non-guix)
+       (url "https://gitlab.com/nonguix/nonguix")))
