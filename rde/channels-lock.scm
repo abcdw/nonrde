d1da262c22411fd3768f5eeafcf555aba6d5b63c
Andrew Tropin <andrew@trop.in>
2025-03-02 10:55:08 +0400

    Update 2024-10-21

--- rde/channels-lock.scm
+++ rde/channels-lock.scm
@@ -1,35 +1,35 @@
 (use-modules (guix channels))
 
 (list (channel
-        (name 'non-guix)
-        (url "https://gitlab.com/nonguix/nonguix")
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
         (commit
-         "143b597422bf01a8fa0e8b4e6e3ece3b3ebe9752")
+         "167f548270bdf032a1f15906d0f39b9c204a72bb")
         (introduction
           (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-         "3d5f4b2d7dda29d749128fbe80f6ad28f52f09b1")
+         "c985075db3e6682d8a5a231c01c770aa5a147f72")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
               "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
+        (name 'non-guix)
+        (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-         "e2e82e878973baee72b40b5b6b35d707f670fc58")
+         "566cf65f9c4b4aec875134904982291461e3d0be")
         (introduction
           (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
